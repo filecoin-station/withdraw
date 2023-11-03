c38192f8c4373625a2e7eefc6e8933932541eb32
Julian Gruber <julian@juliangruber.com>
2023-11-03 18:16:32 +0100

    add passing validation test

--- test.js
+++ test.js
@@ -21,7 +21,6 @@ describe('Withdraw', () => {
   it('withdraws', async () => {
     const balanceOfCalls = []
     const withdrawOnBehalfCalls = []
-
     const contract = {
       balanceOf: async account => {
         balanceOfCalls.push(account)
@@ -83,5 +82,48 @@ describe('Withdraw', () => {
       r,
       s
     }])
+  })
+
+  it('validates signatures', async () => {
+    const balanceOfCalls = []
+    const withdrawOnBehalfCalls = []
+    const contract = {
+      balanceOf: async () => balanceOfCalls.push({}),
+      withdrawOnBehalf: async () => withdrawOnBehalfCalls.push({})
+    }
+    server.once('request', createHandler({ signer, contract }))
+
+    const account = signer.address
+    const nonce = 0
+    const withdraw = signer.address
+    const target = signer.address
+    const value = ethers.utils.parseUnits('1')
+
+    const digest = ethers.utils.solidityKeccak256(
+      ['address', 'uint256', 'address', 'address', 'uint256'],
+      [account, nonce, withdraw, target, value]
+    )
+    const signed = await signer.signMessage(digest)
+    const { v, r, s } = ethers.utils.splitSignature(signed)
+
+    const res = await fetch(`http://127.0.0.1:${server.address().port}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        account,
+        nonce: 1, // invalid nonce
+        target,
+        value: value.toString(),
+        v,
+        r,
+        s
+      })
+    })
+    assert.strictEqual(res.status, 403)
+
+    const message = await res.text()
+    assert.strictEqual(message, 'Invalid signature')
+
+    assert.deepStrictEqual(balanceOfCalls, [])
+    assert.deepStrictEqual(withdrawOnBehalfCalls, [])
   })
 })
